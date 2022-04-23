7de95a2a66844a4df79a8cdae868d8bc49f9ecbd
Gauri Bhand <htcgauribhand@gmail.com>
2022-04-24 00:30:15 +0530

    mongo

--- models/author.js
+++ models/author.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
-const authorSchema = new mongoose.Schema9({
-    name: String,
+const authorSchema = new mongoose.Schema({
+    name:{ 
+    type: String,
     required: true
+    }
 })
 
 module.exports = mongoose.model('Author', authorSchema)
